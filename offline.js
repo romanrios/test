afa894846950710423cbe00be4684fa78e7d7a4d
Román Ríos <romanrios@live.com>
2024-12-05 00:26:32 -0300

    first commit

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1733369059,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/tilemap.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
