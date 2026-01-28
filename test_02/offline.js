75a4141384e2706d92fea21da4fc6f5b36fe99dc
Román Ríos <romanrios@live.com>
2026-01-28 00:33:54 -0300

    test_02

---
+++ test_02/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1769571162,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/mask-sheet0.png",
+		"images/mask-sheet1.png",
+		"images/cuerpo-sheet0.png",
+		"images/cuerpo-sheet1.png",
+		"images/fondo-sheet0.png",
+		"images/image-sheet0.png",
+		"images/1db0a9ef3c7549b5b99ecf52a64a33f-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
